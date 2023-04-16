64c810a7e51f3382160de8dd5304f0f0d43b5ab2
ali <ali.naqvi.dev@gmail.com>
2023-04-15 23:19:20 -0500

    omg outh login in signup is like SUPER FUNCTIONAL !

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -19,54 +19,43 @@ function LoginPage() {
 
 
   useEffect(() => {
-
     let user_obj;
-    const githubCookie = Cookies.get('gh-token')
-    const googleCookie = Cookies.get('google-user')
+    const githubCookie = Cookies.get('gh-token');
+    const googleCookie = Cookies.get('google-user');
 
-    // dispatch(sessionActions.login(googleCookie))
-
-      //GITHUB
-      if (githubCookie) {
-        console.log(githubCookie)
-        axios('https://api.github.com/user', {
-          headers: {
-            'Authorization': `Bearer ${githubCookie}`
-          }
-        })
-          .then(response => {
-            user_obj = { username: response.data.login, firstName: "none", lastName: "none", email: "email@email.com", passsword: 'password' }
-            return dispatch(sessionActions.login(user_obj)).catch(async (res) => {
-              return dispatch(sessionActions.signup(user_obj)).catch(async (res) => {
-                              const data = await res.json()
-              if (data && data.errors.length > 0) {
-                await setErrors(data.errors)
-                // console.log(errors)
-                //   if (data.errors.length == 0) {
-                //     history.push("/rooms/1")
-                //   }
-              }
-            })
-            })
-
-          })
-      } else if (googleCookie) {
-        console.log(JSON.parse(googleCookie))
-        const googleUser = JSON.parse(googleCookie)
-        user_obj = { username: googleUser.displayName, firstName: googleUser.name.givenName, lastName: googleUser.name.familyName, email: "email@email.com", pasword: 'password' }
-        dispatch(sessionActions.login(user_obj))
+    const fetchData = async () => {
+      try {
+        if (githubCookie) {
+          const response = await axios('https://api.github.com/user', {
+            headers: {
+              'Authorization': `Bearer ${githubCookie}`
+            }
+          });
+          user_obj = { username: response.data.login, firstName: "none", lastName: "none", email: "email@email.com", passsword: 'password' };
+        } else if (googleCookie) {
+          const googleUser = JSON.parse(googleCookie);
+          user_obj = { username: googleUser.displayName, firstName: googleUser.name.givenName, lastName: googleUser.name.familyName, email: "email@email.com", pasword: 'password' };
+        }
+        const loginResponse = await dispatch(sessionActions.login(user_obj));
+        history.push("/rooms/1");
+        return loginResponse;
+      } catch (err) {
+        const data = await err.json();
+        if (data && data.errors.length > 0) {
+          setErrors(data.errors);
+          // console.log(errors)
+        }
+        const signupResponse = await dispatch(sessionActions.signup(user_obj));
+        history.push("/rooms/1");
+        return signupResponse;
       }
-    
-  }, [])
+    };
+    fetchData();
 
-  useEffect(() => {
-      //todo make this dry (repeats)
-      const githubCookie = Cookies.get('gh-token')
-      const googleCookie = Cookies.get('google-user')
-
-        //if there are session cookies redirect user if they try to access login page -- until they log out
-
-  }, [])
+    return () => {
+      // cleanup function if needed
+    };
+  }, [dispatch, history]);
 
 
   return (
@@ -75,7 +64,7 @@ function LoginPage() {
       <h1 className='splash-text-h1'>Chatify</h1>
       {errors.length ? errors.map(
         (err) => <p>{err}</p>
-      ): null} 
+      ) : null}
       {/* <p className='splash-text-p'>Imagine a space where you can be a member of a school club, a gaming group, or a worldwide art community. This is a place where you and your small group of friends can spend time together, and where it's easy to stay connected and communicate every day. It's a location that encourages you to hang out more often and fosters a sense of belonging.</p> */}
       <OauthLogin />
 
